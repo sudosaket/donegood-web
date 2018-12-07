2e6dea4a3b5511c1ef09e3010f91c5fa692f0c2a
Michael Forrest <michael.forrest@gmail.com>
2018-12-07 18:22:10 +0000

    set up heroku

--- lib/donegood_web/endpoint.ex
+++ lib/donegood_web/endpoint.ex
@@ -2,7 +2,7 @@ defmodule DonegoodWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :donegood
 
   socket "/socket", DonegoodWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
